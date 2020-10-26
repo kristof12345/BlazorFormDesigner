3833eadcaf239f6dbae578d61f4afbe4ad6cb32c
Kristóf <kristof@Kristof-iMac.local>
2020-10-26 12:24:19 +0100

    Create form, add corrected questions, fix dismiss

---
+++ src/BlazorFormDesigner.BusinessLogic/Models/FormDetails.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace BlazorFormDesigner.Web.Models.DTO
+{
+    public class FormDetails
+    {
+        public string Id { get; set; }
+
+        public List<Question> Questions { get; set; }
+
+        public int NumberOfAnswers { get; set; }
+
+        public double AveragePoints { get; set; }
+
+        public double MaxPoints { get; set; }
+    }
+}
