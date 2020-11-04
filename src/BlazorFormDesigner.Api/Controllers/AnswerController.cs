2ac3e7c442dace8ca65e6f2dd64413a82c8cb4df
Kristóf <kristof@kristof-imac.home>
2020-11-04 14:52:12 +0100

    Export to excel

--- src/BlazorFormDesigner.Api/Controllers/AnswerController.cs
+++ src/BlazorFormDesigner.Api/Controllers/AnswerController.cs
@@ -4,12 +4,16 @@ using BlazorFormDesigner.Web.Models;
 using BlazorFormDesigner.Api.Converters;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace BlazorFormDesigner.Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class AnswerController: AppController
+    public class AnswerController : AppController
     {
         private readonly AnswerService AnswerService;
 
@@ -37,6 +41,30 @@ namespace BlazorFormDesigner.Api.Controllers
             var details = await AnswerService.GetDetails(formid, questionid);
 
             return Ok(details.ToDTO(mapper));
+        }
+
+        [HttpGet]
+        [Route("{formid}")]
+        public async Task<IActionResult> DownloadExcelDocument([FromRoute] string formid)
+        {
+            try
+            {
+                XLWorkbook workbook = await AnswerService.CreateExelDocument(formid);
+
+                string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                string fileName = "answers.xlsx";
+
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, contentType, fileName);
+                }
+            }
+            catch (Exception)
+            {
+                return NoContent();
+            }
         }
     }
 }
--- src/BlazorFormDesigner.BusinessLogic/Services/AnswerService.cs
+++ src/BlazorFormDesigner.BusinessLogic/Services/AnswerService.cs
@@ -1,6 +1,7 @@
 ﻿using BlazorFormDesigner.BusinessLogic.Extensions;
 using BlazorFormDesigner.BusinessLogic.Interfaces;
 using BlazorFormDesigner.BusinessLogic.Models;
+using ClosedXML.Excel;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -65,11 +66,82 @@ namespace BlazorFormDesigner.BusinessLogic.Services
         private async Task<int> calculatePoints(string formid, string questionid, Answer answer)
         {
             var form = await FormRepository.GetById(formid);
+            return calculatePoints(questionid, answer, form);
+        }
+
+        private static int calculatePoints(string questionid, Answer answer, Form form)
+        {
             var correctAnswer = form.Questions.Where(q => q.Id == questionid).First().Options.Where(o => o.IsCorrect).Select(o => o.Content).ToList();
 
             if (ListExtensions.Same(answer.SelectedOptions, correctAnswer)) return 1;
 
             return 0;
+        }
+
+        public async Task<XLWorkbook> CreateExelDocument(string formId)
+        {
+            var form = await FormRepository.GetById(formId);
+            var answers = await AnswerRepository.GetByFormId(formId);
+
+            var workbook = new XLWorkbook();
+
+            CreateSummaryTab(form, answers, workbook);
+            CreateQuestionTabs(form, answers, workbook);
+
+            return workbook;
+        }
+
+        private void CreateSummaryTab(Form form, List<Response> answers, XLWorkbook workbook)
+        {
+            IXLWorksheet worksheet = workbook.Worksheets.Add("Summary");
+            worksheet.Cell(1, 1).Value = "Username";
+            var col = 2;
+            foreach (var q in form.Questions)
+            {
+                worksheet.Cell(1, col++).Value = q.Title;
+            }
+            worksheet.Cell(1, col).Value = "sum";
+
+            for (int index = 1; index <= answers.Count; index++)
+            {
+                var sum = 0;
+                col = 2;
+                worksheet.Cell(index + 1, 1).Value = answers[index - 1].UserId;
+                foreach (var q in form.Questions)
+                {
+                    var points = calculatePoints(q.Id, answers[index - 1].Answers.Where(a => a.QuestionId == q.Id).Single(), form);
+                    worksheet.Cell(index + 1, col++).Value = points;
+                    sum += points;
+                }
+                worksheet.Cell(index + 1, col).Value = sum;
+            }
+        }
+
+        private void CreateQuestionTabs(Form form, List<Response> answers, XLWorkbook workbook)
+        {
+            foreach (var q in form.Questions)
+            {
+                IXLWorksheet worksheet = workbook.Worksheets.Add(q.Title);
+                worksheet.Cell(1, 1).Value = "Username";
+                worksheet.Cell(1, 2).Value = "Answer";
+                for (int index = 1; index <= answers.Count; index++)
+                {
+                    worksheet.Cell(index + 1, 1).Value = answers[index - 1].UserId;
+                    worksheet.Cell(index + 1, 2).Value = WriteOptions(answers[index - 1].Answers.Where(a => a.QuestionId == q.Id).Single().SelectedOptions);
+                }
+            }
+        }
+
+        private string WriteOptions(List<string> selectedOptions)
+        {
+            var result = "";
+
+            for (int i = 0; i < selectedOptions.Count; i++)
+            {
+                result += selectedOptions[i];
+                result += "; ";
+            }
+            return result;
         }
     }
 }
